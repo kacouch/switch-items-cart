6282055d968ea8411cfde19f34c507993302c43f
Jaromir Pirko <jpirko@chemaxon.com>
2018-03-27 00:05:50 +0200

    10 verze

--- src/ItemPage.js
+++ src/ItemPage.js
@@ -3,16 +3,18 @@ import PropTypes from 'prop-types';
 import Item from './Item';
 import './ItemPage.css';
 
-function ItemPage({items, onAddToCart}) {
-    return (
-        <ul className="ItemPage-items">
-            {items.map(item =>
-                <li key={item.id} className="ItemPage-item">
-                    <Item item={item} onAddToCart={() => onAddToCart(item)}/>
-                </li>
-            )}
-        </ul>
-    );
+function ItemPage({ items, onAddToCart }) { return (
+    <ul className="ItemPage-items">
+        {items.map(item =>
+            <li key={item.id} className="ItemPage-item">
+                <Item item={item}>
+                    <button className="Item-addToCart" onClick={() => onAddToCart(item)}> Add to Cart
+                    </button>
+                </Item>
+            </li>
+        )}
+    </ul>
+);
 }
 ItemPage.propTypes = {
     items: PropTypes.array.isRequired,
