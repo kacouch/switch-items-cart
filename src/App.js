f317c18e31a51f576420649dd0bf590ca28326f4
Jaromir Pirko <jpirko@chemaxon.com>
2018-03-26 18:10:13 +0200

    second version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Nav from './Nav';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  renderContent() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <span>Empty</span>
     );
   }
-}
 
-export default App;
+  render() {
+    return (
+        <div className="App">
+          <Nav/>
+          <main className="App-content"> {this.renderContent()}
+          </main>
+        </div>
+    );
+  }
+} export default App;
